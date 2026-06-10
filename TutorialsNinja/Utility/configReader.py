f5bfc8ab57969872f4161418473b7a7c394c4020
Balamurugan <murugan020325@gmail.com>
2026-06-10 09:34:16 +0530

    Refactor config_Read to use relative path for config

--- TutorialsNinja/Utility/configReader.py
+++ TutorialsNinja/Utility/configReader.py
@@ -1,9 +1,10 @@
 from configparser import ConfigParser
-
+import os
 
 def config_Read(category, key):
     config = ConfigParser()
-    config.read(
-        "D:\PythonSeleniumExpleo\PytestFramework\DataDrivenFrameWork\TutorialsNinja\DataProviders\config.ini"
-    )
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    config_path = os.path.join(base_dir, "..", "DataProviders", "config.ini")
+    
+    config.read(config_path)
     return config.get(category, key)
