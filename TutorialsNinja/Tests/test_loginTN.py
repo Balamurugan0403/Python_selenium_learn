73118bd4e262e7b0e50299c4725639f3f2046cdd
Balamurugan <murugan020325@gmail.com>
2026-06-10 09:46:57 +0530

    Refactor login test to improve readability and structure

--- TutorialsNinja/Tests/test_loginTN.py
+++ TutorialsNinja/Tests/test_loginTN.py
@@ -6,19 +6,16 @@ from selenium.webdriver.support.wait import WebDriverWait
 from Utility.excelDP import get_Excel_Data
 from Utility import logCreators
 
-
 @pytest.mark.parametrize(
     "username,password",
     get_Excel_Data(
-        "TutorialsNinja/DataProviders/loginTestTN",
-        "loginTestTN",
+        "DataProviders/loginTN.xlsx", 
+        "loginTestTN",                 
     ),
 )
 @pytest.mark.usefixtures("upAndDown")
 class TestLoginTN:
-
     def test_ValidLogin(self, username, password):
-
         log = logCreators.log_generator()
         wait = WebDriverWait(self.driver, 15)
 
@@ -29,29 +26,21 @@ class TestLoginTN:
 
         wait.until(
             ec.visibility_of_element_located(
-                (
-                    By.XPATH,
-                    "//li[@class='dropdown open']/child::ul/li/a[text()='Login']",
-                )
+                (By.XPATH, "//li[@class='dropdown open']/child::ul/li/a[text()='Login']")
             )
         ).click()
 
         wait.until(
             ec.visibility_of_element_located((By.XPATH, "//input[@name='email']"))
         ).send_keys(username)
-
         log.info("Username Entered")
 
-        self.driver.find_element(By.XPATH, value="//input[@name='password']").send_keys(
-            password
-        )
-
+        self.driver.find_element(By.XPATH, "//input[@name='password']").send_keys(password)
         log.info("Password Entered")
 
         wait.until(
             ec.element_to_be_clickable((By.XPATH, "//input[@type='submit']"))
         ).click()
-
         log.info("Clicked Login Button")
 
         try:
@@ -60,25 +49,17 @@ class TestLoginTN:
                     (By.XPATH, "//div[@id='content']/child::h2[text()='My Account']")
                 )
             ).text
-
-            assert actual.__contains__("My Account")
+            assert "My Account" in actual
             log.info("Login Successful")
 
         except Exception as e:
-
             log.info("Login Failed")
-
             try:
                 error = wait.until(
                     ec.visibility_of_element_located(
-                        (
-                            By.XPATH,
-                            "//div[@id='account-login']/child::ul/following::div[@class='alert alert-danger alert-dismissible']",
-                        )
+                        (By.XPATH, "//div[@id='account-login']/child::ul/following::div[@class='alert alert-danger alert-dismissible']")
                     )
                 ).text
-
                 log.info(error)
-
-            except:
-                log.info(e)
+            except Exception as inner_e:
+                log.info(inner_e)
